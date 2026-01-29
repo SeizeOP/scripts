e1df4c4cbcd182e5805cc32c8c7d0559a1417715
Henry <102833022+SeizeOP@users.noreply.github.com>
2026-01-28 20:02:23 -0500

    update setup.sh

--- bash/setup.sh
+++ bash/setup.sh
@@ -17,7 +17,8 @@ if [[ -f /etc/os-release ]]; then
     . /etc/os-release 
     DISTRO_NAME="$NAME"
     DISTRO_ID="$ID"
-    echo -e "Detected OS: $DISTRO_NAME ($DISTRO_ID)"
+    echo -e "Detected OS: \e[1;32m$DISTRO_NAME\e[0m ($DISTRO_ID)"
+    echo ""
 else
     echo -e "Cannot detect OS version. /etc/os-release not found"
     DISTRO_NAME="unknown"
@@ -26,75 +27,79 @@ fi
 
 # Warn user if script is run on an unsupported distrobution.
 case $DISTRO_ID in
-	unknown)
-		echo -e "Cannot detect OS version. \e[1;31m/etc/os-release\e[0m not found"
-		echo ""
-		echo -e "This script supports ONLY distrobutions based on Fedora, Debian/Ubuntu, OpenSUSE, and Arch Linux." 
-		echo "If you know that your system is based on one of these supported distrobutions you may proceed with running this script."
-		echo "Otherwise, it is reccomended to exit the script now."
-		echo ""
-		echo "Portions of this script will work on unsupported disrobutions. However package installation and update will not work."
-		echo -e "Continue to Run HD's post install script?"
-		select strictreply in "Yes" "No"; do
-			case $relaxedreply in
-				Yes | YES | yes | Y | y )
-					echo "Continuing to load script..."
-					echo "" ; break ;;
-				No | NO | no | n )
-					echo "Exiting script..."
-					kill -9 $(ps aux | grep '[s]etup.sh' | awk '{print $2}')
-				esac
-			case $relaxedreply in
-				* ) 
-					echo "Please answer \e[1;32myes\e[0m or \e[1;31mno\e[0m."; break ;;
-    		esac
-		done
+    unknown)
+	echo -e "Cannot detect OS version. \e[1;31m/etc/os-release\e[0m not found"
+	echo ""
+	echo -e "This script supports ONLY distrobutions based on Fedora, Debian/Ubuntu, OpenSUSE, and Arch Linux." 
+	echo "If you know that your system is based on one of these supported distrobutions you may proceed with running this script."
+	echo "Otherwise, it is reccomended to exit the script now."
+	echo ""
+	echo "Portions of this script will work on unsupported disrobutions. However package installation and update will not work."
+	echo -e "Continue to Run HD's post install script?"
+	select strictreply in "Yes" "No"; do
+	    case $relaxedreply in
+		Yes | YES | yes | Y | y )
+		    echo "Continuing to load script..."
+		    echo "" ; break ;;
+		No | NO | no | n )
+		    echo "Exiting script..."
+		    kill -9 $(ps aux | grep '[s]etup.sh' | awk '{print $2}')
+	    esac
+	    case $relaxedreply in
+		* ) 
+		    echo "Please answer \e[1;32myes\e[0m or \e[1;31mno\e[0m."; break ;;
+    	    esac
+	done
 esac
-
 # Update System
 echo "It is reccomended that the system is fully up to date when running this script."
 echo -e "\e[1;31mCheck for updates now?\e[0m"
-select strictreply in "Yes" "No"; do
+select strictreply in "Yes" "No" "Quit"; do
     relaxedreply=${strictreply:-$REPLY}
     case $relaxedreply in
-		Yes | YES | yes | Y | y )
-			case $DISTRO_ID in
-				arch|endeavoros|garuda|manjaro)
-					echo "Updating Pacman packages..."
-					sudo pacman -Syu
-					echo "" ; break ;;
-				bazzite|ublue|secureblue)
-					echo -e "Updating OSTree..."
-					sudo rpm-ostree upgrade
-					echo -e "\e[1;31mPlease reboot to apply any newly downloaded OSTree\e[0m" 
-					echo "" ; break ;;
-	    		debian|ubuntu|mint|zorin)
-					echo -e "Updating APT packages..."
-					sudo apt update
-					sudo apt upgrade -y 
-					echo "" ; break ;;
-				fedora|redhat)
-					echo -e "Updating RPM packages..." 
-					sudo dnf upgrade --refresh -y 
-					echo "" ; break ;;
-				opensuse-leap)
-					echo -e "Updating Zypper packages..."
-					sudo zypper refresh -y
-					sudo zypper update -y 
-					echo "" ; break ;;
-				opensuse-tumbleweed)
-					echo -e "Updating Zypper packages..."
-					sudo zypper refresh -y
-					sudo zypper dist-upgrade -y 
-					echo "" ; break;;
-			esac
+	Yes | YES | yes | Y | y )
+	    case $DISTRO_ID in
+		arch|endeavoros|garuda|manjaro)
+		    echo "Updating Pacman packages..."
+		    sudo pacman -Syu
+		    echo "" ; break ;;
+		bazzite|ublue|secureblue)
+		    echo -e "Updating OSTree..."
+		    sudo rpm-ostree upgrade
+		    echo -e "\e[1;31mPlease reboot to apply any newly downloaded OSTree\e[0m" 
+		    echo "" ; break ;;
+	    	debian|ubuntu|mint|zorin)
+		    echo -e "Updating APT packages..."
+		    sudo apt update
+		    sudo apt upgrade -y 
+		    echo "" ; break ;;
+		fedora|redhat)
+		    echo -e "Updating RPM packages..." 
+		    sudo dnf upgrade --refresh -y 
+		    echo "" ; break ;;
+		opensuse-leap)
+		    echo -e "Updating Zypper packages..."
+		    sudo zypper refresh -y
+		    sudo zypper update -y 
+		    echo "" ; break ;;
+		opensuse-tumbleweed)
+		    echo -e "Updating Zypper packages..."
+		    sudo zypper refresh -y
+		    sudo zypper dist-upgrade -y 
+		    echo "" ; break;;
+	    esac
+	esac
+    	case $relaxedreply in
+	    Quit | QUIT | quit | Q | q )
+		echo "Exiting script..."
+		kill -9 $(ps aux | grep '[s]etup.sh' | awk '{print $2}')
 	esac
 	case $relaxedreply in
-		No | NO | no | n )
-			echo -e "\e[1;33mskipping system update...\e[0m" 
-			echo "" ; break ;;
-		* ) 
-			echo -e "Please answer \e[1;32myes\e[0m or \e[1;31mno\e[0m."
+	    No | NO | no | n )
+		echo -e "\e[1;33mskipping system update...\e[0m" 
+		echo "" ; break ;;
+	    * ) 
+		echo -e "Please answer \e[1;32myes\e[0m or \e[1;31mno\e[0m."
     esac
 done
 
@@ -107,55 +112,59 @@ echo -e "\e[1;32m2.\e[0m Install an AUR helper on Arch based systems"
 echo -e "\e[1;32m3.\e[0m Enable several PPAs on Ubuntu based systems"
 echo -e "\e[1;32m4.\e[0m Enable several 3rd party repositories on Bazzite/Ublue systems" 
 echo ""
-select strictreply in "Yes" "No"; do
+select strictreply in "Yes" "No" "Quit"; do
     relaxedreply=${strictreply:-$REPLY}
     case $relaxedreply in
-		Yes | YES | yes | Y | y )
-			case $DISTRO_ID in
-				fedora|redhat)
-					# If on Fedora, enable Fedora COPRs
-					sudo dnf copr enable erikreider/SwayNotificationCenter
-					sudo dnf copr enable sdegler/hyprland
-					sudo dnf copr enable yalter/niri
-					# enable brave browser non-COPR repo for fedora
-					sudo dnf install dnf-plugins-core -y
-					sudo config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-					echo "" ; break ;;
-			esac
-		# if on Bazzite enable Brave browser repo
-			case $DISTRO_ID in
-				bazzite|ublue)
-					run0 curl -fsSLo /etc/yum.repos.d/brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-			esac
-		# If on Arch, Install an AUR helper
-			case $DISTRO_ID in
-				arch|endeavoros|garuda|manjaro)		
-					sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si 		
-					echo "" ; break ;;
-			esac
-		# If on Ubuntu enable Ubuntu PPAs, Universe, and Restricted repositories
-			case $DISTRO_ID in
-				debian|ubuntu|mint|zorin)
-					sudo add-apt-repository universe
-					sudo add-apt-repository restricted
-					echo "" ; break ;;
-			esac
-		# If on Opensuse add 3rd party repositories
-			case $DISTRO_ID in
-				opensuse|opensuse-tumbleweed|opensuse-leap)
-					sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-					echo "" ; break ;;
-			esac
+	Yes | YES | yes | Y | y )
+	    case $DISTRO_ID in
+		fedora|redhat)
+		    # If on Fedora, enable Fedora COPRs
+		    sudo dnf copr enable erikreider/SwayNotificationCenter
+		    sudo dnf copr enable sdegler/hyprland
+		    sudo dnf copr enable yalter/niri
+		    # enable brave browser non-COPR repo for fedora
+		    sudo dnf install dnf-plugins-core -y
+		    sudo config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+		    echo "" ; break ;;
+	    esac
+	# if on Bazzite enable Brave browser repo
+	    case $DISTRO_ID in
+		bazzite|ublue)
+		    run0 curl -fsSLo /etc/yum.repos.d/brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+	    esac
+	# If on Arch, Install an AUR helper
+	    case $DISTRO_ID in
+		arch|endeavoros|garuda|manjaro)		
+		    sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si 		
+		    echo "" ; break ;;
+	    esac
+	# If on Ubuntu enable Ubuntu PPAs, Universe, and Restricted repositories
+	    case $DISTRO_ID in
+		debian|ubuntu|mint|zorin)
+		    sudo add-apt-repository universe
+		    sudo add-apt-repository restricted
+		    echo "" ; break ;;
+	    esac
+	# If on Opensuse add 3rd party repositories
+	    case $DISTRO_ID in
+		opensuse|opensuse-tumbleweed|opensuse-leap)
+		    sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+		    echo "" ; break ;;
+	    esac
 	esac
-	case $relaxedreply in
-		No | NO | no | n )  
-			echo -e "\e[1;33mSkipping 3rd party repo setup...\e[0m"
-			echo "" ; break ;;
-		* )
-			echo -e "Please answer \e[1;32myes\e[0m or \e[1;31mno\e[0m."
-	esac
+    case $relaxedreply in
+	No | NO | no | n )  
+	    echo -e "\e[1;33mSkipping 3rd party repo setup...\e[0m"
+	    echo "" ; break ;;
+	* )
+	    echo -e "Please answer \e[1;32myes\e[0m or \e[1;31mno\e[0m."
+    esac
+    case $relaxedreply in
+	Quit | QUIT | quit | Q | q )
+	    echo "Exiting script..."
+	    kill -9 $(ps aux | grep '[s]etup.sh' | awk '{print $2}')
+    esac
 done
-
 ### Install Dependencies ###
 apt_packages=(git sway swaylock swaync waybar waypaper wlogout kitty rofi-wayland emacs tealdeer)
 aur_packages=(brave-bin swaync waypaper wlogout libtool libvterm)
@@ -251,32 +260,37 @@ else
 fi
 ### Clone the required git repositories ###
 echo "Install preconfigured dotfiles?"
-select strictreply in "Yes" "No"; do
+select strictreply in "Yes" "No" "Quit"; do
     relaxedreply=${strictreply:-$REPLY}
     case $relaxedreply in
-		Yes | YES | yes | Y | y ) 
-	    	git clone https://github.com/SeizeOP/dots.git ~/dotfiles/ 
-			echo "" ; break ;;
-				
-		No | NO | no | n )
-			echo -e "\e[1;33mSkipping download of preconfigured dotfiles...\e[0m" 
-			echo "" ; break ;;
-		* )
-			echo -e "Please answer \e[1;32myes\e[0m or \e[1;31mno\e[0m."
+	Quit | QUIT | quit | Q | q )
+	    echo "Exiting script..."
+	    kill -9 $(ps aux | grep '[s]etup.sh' | awk '{print $2}') ; break ;;
+	Yes | YES | yes | Y | y ) 
+	    git clone https://github.com/SeizeOP/dots.git ~/dotfiles/ 
+	    echo "" ; break ;;		
+	No | NO | no | n )
+	    echo -e "\e[1;33mSkipping download of preconfigured dotfiles...\e[0m" 
+	    echo "" ; break ;;
+	* )
+	    echo -e "Please answer \e[1;32myes\e[0m or \e[1;31mno\e[0m."
     esac
 done
 echo "Woud you like to the install additional shell scripts to add system functionality?"
 echo ""
 echo -e "\e[1;31mNOTE:\e[0m Required for some Wlogout functionality, and launch scripts."
-select strictreply in "Yes" "No"; do
+select strictreply in "Yes" "No" "Quit"; do
     relaxedreply=${strictreply:-$REPLY}
     case $relaxedreply in
-		Yes | YES | yes | Y | y ) 
-	    	git clone https://github.com/SeizeOP/scripts.git ~/scripts/ ; break ;;
-		No | NO | no | n )
-			echo -e "Skipping download of scripts repo..." ; break ;;
-		* )
-			echo -e "Please answer \e[1;32myes\e[0m or \e[1;31mno\e[0m."
+	Quit | QUIT | quit | Q | q )
+	    echo "Exiting script..."
+	    kill -9 $(ps aux | grep '[s]etup.sh' | awk '{print $2}') ; break ;;
+	Yes | YES | yes | Y | y ) 
+	    git clone https://github.com/SeizeOP/scripts.git ~/scripts/ ; break ;;
+	No | NO | no | n )
+	    echo -e "Skipping download of scripts repo..." ; break ;;
+	* )
+	    echo -e "Please answer \e[1;32myes\e[0m or \e[1;31mno\e[0m."
     esac
 done
  
@@ -289,7 +303,7 @@ select strictreply in "Yes" "No"; do
     case $relaxedreply in
 	Yes | YES | yes | Y | y ) 
 	    ln -sf ~/dotfiles/emacs/ -t ~/.config/
-		ln -sf ~/dotfiles/hypr/ -t ~/.config/ 
+	    ln -sf ~/dotfiles/hypr/ -t ~/.config/ 
 	    ln -sf ~/dotfiles/niri/ -t ~/.config/ 
 	    ln -sf ~/dotfiles/nwg-wrapper/ -t ~/.config/ 
 	    ln -sf ~/dotfiles/rofi/ -t ~/.config/ 
@@ -391,6 +405,5 @@ select strictreply in "Yes" "No"; do
 		echo -e "Please answer \e[1;32myes\e[0m or \e[1;31mno\e[0m."
     esac
 done
-
 echo "Thank you for using HD's Post install script."
 echo "A reeboot may be necessary to fully apply changes made by this script."
